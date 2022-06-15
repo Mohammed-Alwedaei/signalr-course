dd0ad8d52bb985008c7d8de14cf10d862f0b007d
Mohammed Alwedaei <mohammed.alwedaei@outlook.com>
2022-06-15 12:50:16 +0300

    Added page count and users count

--- SignalR.Web/Hubs/UserHub.cs
+++ SignalR.Web/Hubs/UserHub.cs
@@ -4,5 +4,29 @@ namespace SignalR.Web.Hubs
 {
     public class UserHub : Hub
     {
+        public static int TotalPageView { get; set; } = 0;
+        public static int TotalPageUsers { get; set; } = 0;
+
+        //To be called when the connection is established successfully 
+        public override async Task OnConnectedAsync()
+        {
+            TotalPageUsers++;
+            await Clients.All.SendAsync("updatePageUsers", TotalPageUsers);
+        }
+
+        //Will be called when the connection is disconnected
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalPageUsers--;
+            await Clients.All.SendAsync("updatePageUsers", TotalPageUsers);
+        }
+
+        public async Task UpdatePageViews()
+        {
+            TotalPageView++;
+
+            //Send a request for the corresponding clients
+            await Clients.All.SendAsync("updatePageViews", TotalPageView);
+        }
     }
 }
---
+++ SignalR.Web/wwwroot/js/liveUserCountApp.js
@@ -0,0 +1,37 @@
+﻿//Create the connection
+const connectionBuilder = new signalR.HubConnectionBuilder()
+    .withUrl("/Hubs/UserCount")
+    .build();
+
+//Make a call the hub
+connectionBuilder.on("updatePageViews",
+    value => {
+        document.querySelector("#display-live-count").innerText = value.toString();
+    });
+
+connectionBuilder.on("updatePageUsers",
+    value => {
+        document.querySelector("#display-live-users-count").innerText = value.toString();
+    });
+
+//Send the client data to the hub
+const onNewPageLoad = () => {
+    connectionBuilder.send("UpdatePageViews");
+    connectionBuilder.send("UpdatePageUsers");
+}
+
+const onFulfill = () => {
+    console.log("Connection has been established (success)");
+
+    //Update page count
+
+    onNewPageLoad();
+}
+
+const onReject = () => {
+    console.log("Connection has not been established (rejected)");
+}
+
+//Establish the connection
+connectionBuilder.start().then(onFulfill, onReject);
+
